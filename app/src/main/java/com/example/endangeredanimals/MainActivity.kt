6e6ad37880d6ab8470b3b599966563f945e54ffd
eyadamr905 <eyadamro905@gmail.com>
2024-08-26 14:43:23 +0300

    final

--- app/src/main/java/com/example/endangeredanimals/MainActivity.kt
+++ app/src/main/java/com/example/endangeredanimals/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.example.endangeredanimals
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -16,14 +18,22 @@ import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.SpanStyle
@@ -33,6 +43,7 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.core.net.toUri
 import com.example.endangeredanimals.data.DataSource
 import com.example.endangeredanimals.model.Animal
 import com.example.endangeredanimals.ui.theme.CatskillWhite
@@ -45,7 +56,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             EndangeredAnimalsTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                        AnimalsList(DataSource().getAnimalsList(),Modifier.padding(innerPadding))
+                    AnimalsList(DataSource().getAnimalsList(), Modifier.padding(innerPadding))
                 }
             }
         }
@@ -53,9 +64,52 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
+fun AnimalHelperDialog(animal: Animal, onSHowDialog: (Boolean) -> Unit) {
+val context = LocalContext.current
+    AlertDialog(
+        containerColor = CatskillWhite,
+        onDismissRequest = { },
+        confirmButton = {
+            TextButton(
+                onClick = {
+                val i = Intent(Intent.ACTION_VIEW,animal.link.toUri())
+                    context.startActivity(i)
+                    onSHowDialog(false)
+                }
+            ) { Text(text = stringResource(R.string.proceed)) }
+        },
+        dismissButton = {
+            TextButton(
+                onClick = { onSHowDialog(false) }
+            ) { Text(text = stringResource(R.string.cancel)) }
+        },
+        icon = {
+            Icon(
+                painter = painterResource(id = R.drawable.baseline_help_24),
+                contentDescription = stringResource(R.string.help)
+            )
+        },
+        title = {
+            Text(
+                text = stringResource(
+                    id = R.string.save_the_animal,
+                    stringResource(id = R.string.save),
+                    stringResource(id = R.string.the_animal),
+                    '?'
+                )
+            )
+        },
+        text = {
+            Text(text = stringResource(id = animal.description))
+        }
+    )
+}
+
+
+@Composable
 fun AnimalsList(animals: List<Animal>, modifier: Modifier) {
     LazyColumn {
-        items(animals){
+        items(animals) {
             AnimalListItem(animal = it)
         }
     }
@@ -69,7 +123,11 @@ private fun AnimalsListPreview() {
 
 @Composable
 fun AnimalListItem(animal: Animal, modifier: Modifier = Modifier) {
+    var isDialogShown by remember { mutableStateOf(false) }
 
+    if (isDialogShown) AnimalHelperDialog(animal = animal) {
+        isDialogShown = it
+    }
     Card(
         colors = CardDefaults.cardColors(containerColor = CatskillWhite),
         modifier = modifier
@@ -118,6 +176,9 @@ fun AnimalListItem(animal: Animal, modifier: Modifier = Modifier) {
                             )
                         )
                     },
+                    modifier = modifier.clickable {
+                        isDialogShown = true
+                    }
                 )
             }
         }
